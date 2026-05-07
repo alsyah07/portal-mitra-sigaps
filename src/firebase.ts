dba80ce1d998367405d6cce1582229c9529173d7
alsyah07 <46066495+alsyah07@users.noreply.github.com>
2026-05-07 16:56:48 +0700

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmo5GfitMtwH9sqH190meLv7eZaKxIr9M",
-  authDomain: "sigap-apps-v3-driver.firebaseapp.com",
-  projectId: "sigap-apps-v3-driver",
-  storageBucket: "sigap-apps-v3-driver.firebasestorage.app",
-  messagingSenderId: "1059694642293",
-  appId: "1:1059694642293:web:734c685a97be39349b243c",
-  measurementId: "G-NGEKBF717S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
